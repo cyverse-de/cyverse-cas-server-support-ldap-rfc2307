770cbdaec2ebc44d3fc70de9fdfe1cee6c61b018
Sarah Roberts <sarahr@cyverse.org>
2018-03-02 16:05:05 -0700

    added a project.clj file

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject org.cyverse/dataone-pid-service "1.0.0"
+  :description "Plugin for the DataONE member node service."
+  :license {:name "BSD"
+            :url "http://www.cyverse.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apereo.cas/cas-server-core-authentication "5.2.0" :exclusions [com.nimbusds/lang-tag]]
+                 [org.apereo.cas/cas-server-core-configuration "5.2.0"]
+                 [org.apereo.cas/cas-server-support-ldap "5.2.0" :exclusions [com.nimbusds/lang-tag]]]
+  :source-paths ["src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :javac-options ["-target" "1.8" "-source" "1.8"]
+  :aot :all)
