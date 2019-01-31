f3934f0070156e2bc8b25497a56abb2e193242b5
Sarah Roberts <sarahr@cyverse.org>
2019-01-30 17:07:17 -0700

    CORE-67: migrate the library to CAS 5.3.7

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject org.cyverse/cyverse-cas-server-support-ldap-rfc2307 "1.0.2-SNAPSHOT"
+(defproject org.cyverse/cyverse-cas-server-support-ldap-rfc2307 "5.3.7-SNAPSHOT"
   :description "Plugin for the DataONE member node service."
   :license {:name "BSD"
             :url "http://www.cyverse.org/sites/default/files/iPLANT-LICENSE.txt"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apereo.cas/cas-server-core-authentication "5.2.0" :exclusions [com.nimbusds/lang-tag]]
-                 [org.apereo.cas/cas-server-core-configuration "5.2.0"]
-                 [org.apereo.cas/cas-server-support-ldap "5.2.0" :exclusions [com.nimbusds/lang-tag]]]
+                 [org.apereo.cas/cas-server-core-authentication "5.3.7" :exclusions [com.nimbusds/lang-tag]]
+                 [org.apereo.cas/cas-server-core-configuration "5.3.7"]
+                 [org.apereo.cas/cas-server-support-ldap "5.3.7" :exclusions [com.nimbusds/lang-tag]]]
   :source-paths ["src/main/clojure"]
   :resource-paths ["src/main/resources"]
   :java-source-paths ["src/main/java"]
