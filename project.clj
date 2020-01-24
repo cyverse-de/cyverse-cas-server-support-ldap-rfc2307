1ce97126f175a79bd08e72d016dcb6018e591461
Sarah Roberts <sarahr@cyverse.org>
2020-01-24 16:46:03 -0700

    Version 6.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/cyverse-cas-server-support-ldap-rfc2307 "6.1.3"
+(defproject org.cyverse/cyverse-cas-server-support-ldap-rfc2307 "6.1.4-SNAPSHOT"
   :description "Plugin for the DataONE member node service."
   :license {:name "BSD"
             :url "http://www.cyverse.org/sites/default/files/iPLANT-LICENSE.txt"}
